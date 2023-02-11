2519d4b1c3c8dd869c43d110e0536004293cc7cc
PranayZalke <pranayzalke111@gmail.com>
2023-02-11 11:42:13 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDBUCEpJEZztXE7RGUyPD6hVyt_eP8I9UM",
+  authDomain: "linkedin-clone-65b1b.firebaseapp.com",
+  projectId: "linkedin-clone-65b1b",
+  storageBucket: "linkedin-clone-65b1b.appspot.com",
+  messagingSenderId: "758512601241",
+  appId: "1:758512601241:web:b35f99eaf9dbfe43955008",
+  measurementId: "G-17JN2GT5N7"
+};
+  //this special line of code here connects everything
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  //get the firebase databse
+  const db=firebaseApp.firestore();
+  const auth = firebase.auth();
+  export { db,auth }
